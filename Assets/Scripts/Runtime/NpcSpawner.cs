14b4200f1f4062e9b606dee1f82dde4419c4c810
TalAzaria <talazaria2@gmail.com>
2026-01-30 14:39:15 +0200

    UI render order will worked based on Y axis of npc

--- Assets/Scripts/Runtime/NpcSpawner.cs
+++ Assets/Scripts/Runtime/NpcSpawner.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NpcSpawner : MonoBehaviour
 {
+    public static NpcSpawner Instance;
     public GameManager gameManager;
     public Transform topSpawnPoint, bottomSpawnPoint;
     public GameObject npcPrefab;
     private BoxCollider2D npcCollider;
 
-    [SerializeField] private float _spawnFrequency;
+    private float _spawnFrequency;
 
     private void Awake()
     {
@@ -32,18 +34,31 @@ public class NpcSpawner : MonoBehaviour
     {
         MaskData mask = gameManager.CreateNpcMask(accepted: true);
 
-        var go = Instantiate(npcPrefab, GetNpcSpawnerLocation(), Quaternion.identity);
-        var npc = go.GetComponent<NpcController>();
+        GameObject instantiatedNpcPrefab = Instantiate(npcPrefab, GetNpcSpawnerLocation(), Quaternion.identity);
+        NpcController npcController = instantiatedNpcPrefab.GetComponent<NpcController>();
 
         string dbg = gameManager.BuildMaskDebugString(mask, showAll: true);
         float npcRandomMoveSpeed = Random.Range(gameManager.CurrentLevel.npcMoveMinSpeed, gameManager.CurrentLevel.npcMoveMaxSpeed);
-        npc.Init(npcRandomMoveSpeed, mask, true, dbg);
+        SetOrderInLayerForNpc(instantiatedNpcPrefab);
+        npcController.Init(npcRandomMoveSpeed, mask, true, dbg);
+    }
+
+    public void SetOrderInLayerForNpc(GameObject npc)
+    {
+        SpriteRenderer[] sprites = npc.GetComponentsInChildren<SpriteRenderer>();
+        float uiOrderInLayer = (npc.transform.position.y - bottomSpawnPoint.position.y) / 
+                                (topSpawnPoint.position.y - bottomSpawnPoint.position.y);
+        foreach (SpriteRenderer sprite in sprites)
+        {
+            sprite.sortingOrder = (int)((1 - uiOrderInLayer) * 100);
+        }
+
     }
 
     private Vector3 GetNpcSpawnerLocation()
     {
         float spawnVerticalValue = Random.Range(topSpawnPoint.position.y, bottomSpawnPoint.position.y);
-        spawnVerticalValue -= (spawnVerticalValue % (npcCollider.size.y / 2) + Random.Range(npcCollider.size.y * -0.1f, npcCollider.size.y * 0.1f));
+        spawnVerticalValue -= (spawnVerticalValue % (npcCollider.size.y / 2));
         float spawnHorizontalValue = Random.Range(topSpawnPoint.position.x, bottomSpawnPoint.position.x);
         Vector3 randomSpawnLocation = new Vector3(spawnHorizontalValue, spawnVerticalValue, 0);
         return randomSpawnLocation;
