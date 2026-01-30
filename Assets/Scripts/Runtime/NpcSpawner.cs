9d259614a6f26ee3ddb8c5d4cea3bd169766e3d1
TalAzaria <talazaria2@gmail.com>
2026-01-30 11:41:35 +0200

    Added sceene changes and fixed merge conflicts in NpcSpawner

--- Assets/Scripts/Runtime/NpcSpawner.cs
+++ Assets/Scripts/Runtime/NpcSpawner.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 public class NpcSpawner : MonoBehaviour
 {
     public GameManager gameManager;
+    public Transform topSpawnPoint, bottomSpawnPoint;
     public GameObject npcPrefab;
-    public Transform spawnPoint;
+    private BoxCollider2D npcCollider;
 
-    private float _t;
+    [SerializeField] private float _spawnFrequency;
+
+    private void Awake()
+    {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        npcCollider = npcPrefab.GetComponent<BoxCollider2D>();
+    }
 
     private void Update()
     {
         if (!gameManager || !gameManager.IsRunning) return;
 
-        _t += Time.deltaTime;
-        if (_t >= gameManager.CurrentLevel.npcDoorInterval)
+        _spawnFrequency += Time.deltaTime;
+        if (_spawnFrequency >= gameManager.CurrentLevel.npcDoorInterval)
         {
-            _t = 0f;
-            SpawnNpcLevel1();
+            _spawnFrequency = 0f;
+            CreateNpcLevel1();
         }
     }
 
-    private void SpawnNpcLevel1()
+    private void CreateNpcLevel1()
     {
-        // Level 1: everyone accepted; relevant features always identical to rule.
-        var mask = gameManager.CreateNpcMask(accepted: true);
+        MaskData mask = gameManager.CreateNpcMask(accepted: true);
 
-        var go = Instantiate(npcPrefab, spawnPoint.position, Quaternion.identity);
+        var go = Instantiate(npcPrefab, GetNpcSpawnerLocation(), Quaternion.identity);
         var npc = go.GetComponent<NpcController>();
 
         string dbg = gameManager.BuildMaskDebugString(mask, showAll: true);
-        npc.Init(gameManager.CurrentLevel.npcMoveSpeed, mask, true, dbg);
+        float npcRandomMoveSpeed = Random.Range(gameManager.CurrentLevel.npcMoveMinSpeed, gameManager.CurrentLevel.npcMoveMaxSpeed);
+        npc.Init(npcRandomMoveSpeed, mask, true, dbg);
+    }
+
+    private Vector3 GetNpcSpawnerLocation()
+    {
+        float spawnVerticalValue = Random.Range(topSpawnPoint.position.y, bottomSpawnPoint.position.y);
+        spawnVerticalValue -= (spawnVerticalValue % (npcCollider.size.y / 2) + Random.Range(npcCollider.size.y * -0.1f, npcCollider.size.y * 0.1f));
+        float spawnHorizontalValue = Random.Range(topSpawnPoint.position.x, bottomSpawnPoint.position.x);
+        Vector3 randomSpawnLocation = new Vector3(spawnHorizontalValue, spawnVerticalValue, 0);
+        return randomSpawnLocation;
     }
 }
